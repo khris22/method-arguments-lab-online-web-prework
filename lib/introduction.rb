eb5bd6b14ff1c93baa88a8af2e03641d60d36cab
Khristian Punzalan <khrispunzalan@gmail.com>
2019-05-10 13:04:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,11 @@
 # lib/introduction.rb
  
 def introduction(name)
-    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+    puts "Hi, my name is #{name}."
+end
+
+#introduction_with_language
+
+def introduction_with_language
+and I am learning to program in #{language}."
 end
